b10b142bde01fddf2faac694ed87a9f95f8e18b8
magicdawn@qq.com <magicdawn@qq.com>
2016-11-25 14:31:05 +0800

    ract: add more lexer test

--- test/lexer.js
+++ test/lexer.js
@@ -1,8 +1,10 @@
+'use strict'
+
 const Lexer = require('../lib/lexer.js')
+const RactSyntaxError = require('../lib/ractSyntaxError.js')
 
 describe('Lexer', function() {
-
-  it('lex', function() {
+  it('#lex', function() {
     const input = `
     <html>
       <!--html comment-->
@@ -28,12 +30,60 @@ describe('Lexer', function() {
 
       {{include hello}}
       {{extend foo}}
+      {{block name}}
+
+      {{#block name}}
+      {{/block}}
+      {{#prepend name}}
+      {{/prepend}}
+      {{#append name}}
+      {{/prepend}}
     </html>
     `
     const filename = 'fake.filename'
-
     const lexer = new Lexer(input, filename)
     const tokens = lexer.lex()
     tokens.should.be.ok()
   })
+
+  it('invalid closeSection', () => {
+    const input = '{{/foo}}'
+    const filename = 'fake.filename'
+    try {
+      const tokens = new Lexer(input, filename).lex()
+    } catch (e) {
+      // throw e
+      e.should.instanceOf(RactSyntaxError)
+      e.message.should.equal('invalid closeSection')
+    }
+  })
+
+  it('bad expression', () => {
+    const input = '{{#if @}}{{/if}}'
+    const filename = 'fake.filename'
+    try {
+      const tokens = new Lexer(input, filename).lex()
+    } catch (e) {
+      e.should.instanceOf(RactSyntaxError)
+      e.message.should.equal('bad expression')
+    }
+  })
+
+  it('bad js code', () => {
+    const input = '{{- hello world}}'
+    const filename = 'fake.filename'
+    try {
+      const tokens = new Lexer(input, filename).lex()
+    } catch (e) {
+      e.should.instanceOf(RactSyntaxError)
+      e.message.should.equal('bad js code')
+    }
+  })
+
+  // it('eos token', () => {
+  //   const input = '{{name}}'
+  //   const filename = 'fake.filename'
+  //   const tokens = new Lexer(input, filename).lex()
+  //   console.log(tokens)
+  // })
 })
